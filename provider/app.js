59b652423e9aad9178e533d6b483720e7fd4ce18
Luis Ortega <luis.ortega@rappi.com>
2023-03-25 13:50:45 -0500

    mock provider backend

---
+++ provider/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const c = require('./db')
+const app = express()
+const port = 3000
+
+app.get('/getAllSkuOffers/:sku', (req, res) => {
+    const { sku } = req.params;
+    const [ offers ] = c.DB_ROWS.filter(p => p.sku === sku)
+    res.json(offers ?? { "error": "Not found valid offers" });
+})
+
+app.listen(port, () => {
+   console.log(`Provider mock listening on port ${port}`)
+})
